f46b6c2c5a691756b8eac28029400932532a2913
shun shimono <68690488+shunshimono@users.noreply.github.com>
2022-12-20 03:10:19 +0900

    navi途中まで

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -5,6 +5,39 @@ import { Link } from "gatsby"
 
 import { ClosedMenu, OpendMenu } from "../../icons"
 
+const links = [
+  {
+    to: "/about",
+    label: "私たちについて",
+    displayValue: "about",
+  },
+  {
+    to: "/news",
+    label: "ニュース",
+    displayValue: "news",
+  },
+  {
+    to: "/recruit",
+    label: "採用情報",
+    displayValue: "recruit",
+  },
+  {
+    to: "/service",
+    label: "サービスについて",
+    displayValue: "service",
+  },
+  {
+    to: "/info",
+    label: "会社概要",
+    displayValue: "info",
+  },
+  {
+    to: "/from",
+    label: "お問合せ",
+    displayValue: "from",
+  },
+]
+
 export const Header = ({ siteTitle }: any) => {
   const [openMenu, setOpenMenu] = useState(false)
 
@@ -21,6 +54,19 @@ export const Header = ({ siteTitle }: any) => {
         <HamburgerMenu name="メニュー" type="button" onClick={onClickMenu}>
           {openMenu ? <OpendMenu /> : <ClosedMenu />}
         </HamburgerMenu>
+        <MenuLink openMenu={openMenu}>
+          <nav>
+            {links.map(v => {
+              return (
+                <li>
+                  <Link to={v.to} aria-label={v.label}>
+                    {v.displayValue}
+                  </Link>
+                </li>
+              )
+            })}
+          </nav>
+        </MenuLink>
       </Wrapper>
     </NaviHeader>
   )
@@ -37,10 +83,12 @@ const NaviHeader = styled.header`
 
 const Wrapper = styled.div<{ openMenu: boolean }>`
   display: flex;
+  position: relative;
   justify-content: space-between;
   align-items: center;
   height: 100%;
   padding: 0 1.6rem;
+  transition: all 0.25s;
   ${({ openMenu }) =>
     openMenu &&
     css`
@@ -54,4 +102,21 @@ const HamburgerMenu = styled.button`
     width: 20px;
     height: 20px;
   }
+`
+
+const MenuLink = styled.div<{ openMenu: boolean }>`
+  position: absolute;
+  top: 76px;
+  background: blue;
+  width: 100%;
+  left: 0;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.5s;
+  ${({ openMenu }) =>
+    openMenu &&
+    css`
+      visibility: visible;
+      opacity: 1;
+    `}
 `
