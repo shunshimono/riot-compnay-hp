b6fb7c1a7c4f7c6c25b5bbcff0ef437ae7539577
shun shimono <68690488+shunshimono@users.noreply.github.com>
2022-12-20 20:06:27 +0900

    主要事業作成

--- src/components/service/index.tsx
+++ src/components/service/index.tsx
@@ -1,15 +1,79 @@
 import React from "react"
 import styled from "@emotion/styled"
+import { StaticImage } from "gatsby-plugin-image"
+import { COLOR } from "../constant/color"
 
 export const Service = () => {
   return (
     <Wrapper>
-      <Card></Card>
-      <Card></Card>
+      <Heading>主要事業</Heading>
+      <Card>
+        <StaticImage
+          src="../../images/team.jpg"
+          alt="チーム開発"
+          placeholder="blurred"
+          loading="lazy"
+          className="static-image"
+        />
+        <Title>受託開発事業</Title>
+        <Description>
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </Description>
+      </Card>
+      <Card>
+        <StaticImage
+          src="../../images/ses.jpg"
+          alt="SES"
+          placeholder="blurred"
+          loading="lazy"
+          className="static-image"
+        />
+        <Title>SES事業</Title>
+        <Description>
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </Description>
+      </Card>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  padding: 40px 15px;
+`
 
-const Card = styled.div``
+const Heading = styled.div`
+  position: relative;
+  padding-left: 20px;
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 0.05em;
+  position: relative;
+  ::before {
+    content: "";
+    line-height: 1.6;
+    display: block;
+    position: absolute;
+    width: 3px;
+    height: 28px;
+    left: 0;
+    top: 0;
+    background-color: #c2a47e;
+  }
+`
+
+const Card = styled.div`
+  margin-bottom: 20px;
+  padding: 30px;
+  background: ${COLOR.LIGHT.code};
+`
+const Title = styled.div`
+  margin-top: 1.6em;
+  font-weight: bold;
+  font-size: 16px;
+`
+
+const Description = styled.div`
+  margin-top: 1.4em;
+  font-size: 14px;
+  font-weight: 300;
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { Top } from "../components/top"
+import { Service } from "../components/service"
 
 const IndexPage = () => (
   <Layout>
     <Top></Top>
+    <Service></Service>
   </Layout>
 )
 
